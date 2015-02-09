966e679ffdce59098cb8439a13d586ebfeb30318
LetayB <sun0post@gmail.com>
2015-02-09 11:53:34 +0100

    color add

--- src/Gtk/Jouer.rb
+++ src/Gtk/Jouer.rb
@@ -10,13 +10,17 @@ class Jouer
 		vbox = Gtk::VBox.new(false,10)
 
 	
-		@plateau = Plateau.new("_1_0");
-		@plateauGtk = PlateauGtk.creer(vbox,@plateau,2)
+		@plateau = Plateau.new("___1____1_1____________000_____________1____0_________0_____1______0__1_1___0____11________1_0__1__0_1_______0____0________0___0__0_1___0___1___");
+		@plateauGtk = PlateauGtk.creer(vbox,@plateau,12)
 		
 		boutonRetour.signal_connect('clicked'){
 			fenetre.remove(vbox)
 			Menu.afficher(fenetre, langue)
 		}
+		
+
+
+		vbox.add(@plateauGtk.table)
 
 		vbox.add(boutonRetour)
 		
