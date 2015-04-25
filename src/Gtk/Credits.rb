2e6f637e9e5b4a5db1151ade939650553adf898f
godefroy29 <godefroy29@gmail.com>
2015-04-25 12:21:55 +0200

    correction de Crédit

--- src/Gtk/Credits.rb
+++ src/Gtk/Credits.rb
@@ -20,31 +20,6 @@ class Credits
 		vbox.add(label)
 		vbox.add(boutonRetour)
 
-
-
-		pix_blue = Gdk::Pixbuf.new("blue.png",200,200)
-		pix_red = Gdk::Pixbuf.new("red.png",200,200)
-		red = Gtk::Image.new(pix_red)
-		blue = Gtk::Image.new(pix_blue)
-
-		btn_tmp = Gtk::Button.new("vide")
-		btn_tmp.image = blue
-
-		vbox.add(btn_tmp)
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
 		fenetre.add(vbox)
 		fenetre.show_all
 	end
