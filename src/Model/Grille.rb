19043505d65f55d3e782bd25f18d6c76133c429d
Menkusso <Menkusso@users.noreply.github.com>
2015-04-29 21:12:25 +0200

    Ajout de commentaires

--- src/Model/Grille.rb
+++ src/Model/Grille.rb
@@ -1,19 +1,22 @@
+#Classe qui permet de stocker les informations concernant les grilles de la base de donnée
 class Grille
 
-	attr_reader :id
-	attr_reader :taille
-	attr_reader :difficulte
-	attr_reader :base
-	attr_reader :solution
+	attr_reader :id			#id de la grille
+	attr_reader :taille		#taille de la grille (longueur et largeur)
+	attr_reader :difficulte		#difficulte de la grille (entier)
+	attr_reader :base		#état initial de la grille (string)
+	attr_reader :solution		#état final de la grille (string)
 
 	def initialize(id,taille,difficulte,base,solution)
-		@id			=	id
+		@id		=	id
 		@taille		=	taille
 		@difficulte	=	difficulte
 		@base		=	base
 		@solution	=	solution
 	end
 
+	##
+	#Méthode d'affichage de la classe
 	def to_s
 		return "Grille id = #{@id} , solution = #{@solution}"
 	end
