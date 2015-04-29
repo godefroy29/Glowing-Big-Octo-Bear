8a278a7086acee8aa29aa669b8e7a334b0bb94a9
sylvain loizeau <spi3028@ic2s114-03.univ-lemans.fr>
2015-04-29 17:23:54 +0200

    Ajout des commantaires

--- src/Model/Score.rb
+++ src/Model/Score.rb
@@ -9,7 +9,7 @@ class Score
 	attr_reader :nb_pause
 
 	def initialize(id,joueur,grille,mode,chrono,nb_undo,nb_pause)
-		@id			=	id
+		@id		=	id
 		@joueur		=	joueur
 		@grille		=	grille
 		@mode		=	mode
@@ -18,28 +18,41 @@ class Score
 		@nb_pause	=	nb_pause
 	end
 
+	##
+	# Méthode qui permet de calculer un score
 	def calculScore
 		#TO-DO
-		return 1000 - @chrono - @nb_undo * 10 - @nb_pause * 5
+		return Score.calculScore(@chrono,@nb_undo,@nb_pause)
 	end
 
+	##
+	# Méthode qui permet de calculer un score
 	def Score.calculScore(chrono,nb_undo,nb_pause)
 		#TO-DO
 		return 1000 - chrono - nb_undo * 10 - nb_pause * 5
 	end
 
+	##
+	# Méthode qui ajoute un score dans la base de donnée
 	def Score.ajouteScoreSauvegarde(joueur,grille,chrono,nb_undo,nb_pause)
+		#On vérifie qu'il n'éxiste pas de score pour le joueur dans le mode 0
 		old = ModelScore.getScoreByJoueurAndMode(joueur,0)
+		# Si il en existe 1, on le supprime
 		if old != nil
 			ModelScore.suprScoreById(old.id)
 		end
+		#On ajoute le nouveau score
 		ModelScore.createScore(joueur,grille,0,chrono,nb_undo,nb_pause)
 	end
 
+	##
+	# Méthode qui ajoute un score de partie rapide dans la base de donnée
 	def Score.ajouteScoreRapide(joueur,grille,chrono,nb_undo,nb_pause)
 		ModelScore.createScore(joueur,grille,1,chrono,nb_undo,nb_pause)
 	end
 
+	##
+	# Méthode qui ajoute un score de partie chrono dans la base de donnée
 	def Score.ajouteScoreChrono(joueur,grille,chrono,nb_undo,nb_pause)
 		ModelScore.createScore(joueur,grille,2,chrono,nb_undo,nb_pause)
 	end
