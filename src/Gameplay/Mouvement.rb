90cd837833fc8a18ac0df0ab0fbd486dd65048f0
OffrediE <offredi.etienne@gmail.com>
2015-04-29 21:23:44 +0200

    Update Mouvement.rb

--- src/Gameplay/Mouvement.rb
+++ src/Gameplay/Mouvement.rb
@@ -1,14 +1,16 @@
+#[Classe Mouvement]	Permet d'instancier des changements de couleur de tuile afin de pouvoir les défaire.
 class Mouvement
 
 	@x
 	@y
-	@etatPrecedent
-	@flag
+	@etatPrecedent #Contient une couleur de tuile, spécifiquement la couleur de la case avant son changement
+	@flag #Sert à indiquer (si valant true) que le mouvement est le premier de la pile où il se trouve.
 	
 	attr_reader :x
 	attr_reader :y
 	attr_reader :etatPrecedent
 	attr_reader :flag
+	
 	
 	def initialize(x,y, etat)
 		@x = x
@@ -17,10 +19,16 @@ class Mouvement
 		@flag = false
 	end
 	
+	#[new] Constructeur de la classe Mouvement
+	#<b>paramètre : </b>
+	#* [<i> x </i>] position du changement de couleur
+	#* [<i> y </i>] position du changement de couleur
+	#* [<i> etat </i>] précédente couleur 
 	def Mouvement.enreg(x,y,etat)
 		new(x,y,etat)
 	end
 	
+	#Passe la variable flag à True.
 	def raiseFlag
 		@flag = true
 	end
