8ecc4e0927282bae61d1684f61c2ce555b4f94ef
Menkusso <Menkusso@users.noreply.github.com>
2015-04-30 09:49:33 +0200

    Ajout de commentaires

--- src/Gtk/PartieRapide.rb
+++ src/Gtk/PartieRapide.rb
@@ -1,10 +1,4 @@
-# encoding: UTF-8
-
-##
-# Auteur LOIZEAU Sylvain
-# Version 0.1 : Date : Wed Jan 21 13:47:35 CET 2015
-#
-
+#Classe qui permet de choisir la taille et la difficulté de la grille sur laquelle veut jouer le joueur
 class PartieRapide
 
 	def PartieRapide.afficher(fenetre, langue, mode)
@@ -23,14 +17,17 @@ class PartieRapide
 		comboBoxTaille = Gtk::ComboBox.new()
 		id_grille = nil
 
+		#On affiche toutes les difficultés possibles
 		ModelGrille.getDifficulte().each{ |x|
 			comboBoxDifficulte.append_text(x.to_s)
 		}
 
+		#On affiche toutes les tailles possibles
 		ModelGrille.getTaille().each{ |x|
 			comboBoxTaille.append_text(x.to_s)
 		}
 
+		#On vérifie qu'il existe bien le combo taille+difficulté dans la BdD
 		comboBoxDifficulte.signal_connect('changed'){
 			id_grille = ModelGrille.getGrilleByDifficulteAndTaille(comboBoxDifficulte.active_text.to_i, comboBoxTaille.active_text.to_i)
 			if(id_grille != nil)
@@ -50,6 +47,7 @@ class PartieRapide
 			
 		}
 
+		#On vérifie qu'il existe bien le combo taille+difficulté dans la BdD
 		comboBoxTaille.signal_connect('changed'){
 			id_grille = ModelGrille.getGrilleByDifficulteAndTaille(comboBoxDifficulte.active_text.to_i, comboBoxTaille.active_text.to_i)
 			if(id_grille != nil)
@@ -60,16 +58,19 @@ class PartieRapide
 			
 		}
 
+		#Lance la partie
 		boutonJouer.signal_connect('clicked'){
 			fenetre.remove(vbox)
 			Jouer.afficher(fenetre, langue, mode, id_grille)
 		}
 
+		#Recherche une grille aleatoire
 		boutonAlea.signal_connect('clicked'){
 			fenetre.remove(vbox)
 			Jouer.afficher(fenetre, langue, mode, (Random.new(Time.now.sec).rand(1..7000)))#get alea
 		}
 
+		#Retourne au menu
 		boutonRetour.signal_connect('clicked'){
 			fenetre.remove(vbox)
 			Menu.afficher(fenetre, langue)
@@ -86,6 +87,7 @@ class PartieRapide
 		
 		comboBoxDifficulte.set_active(0);
 		comboBoxTaille.set_active(0);
+		
 		# Ajout des boutons a la vbox
 		vbox.pack_start(labelDifficulte, false, false, padding)
 		vbox.pack_start(comboBoxDifficulte, false, false, padding)
@@ -98,8 +100,6 @@ class PartieRapide
 		vbox.pack_start(boutonQuitter, false, false, padding)
 
 		boutonSave.set_sensitive(false)
-
-
 
 		if !(ModelJoueur.testAnon($joueur))
 			old = ModelScore.getScoreByJoueurAndMode($joueur.id,0)
@@ -114,4 +114,4 @@ class PartieRapide
 		fenetre.reshow_with_initial_size
 	end
 
-end # Marqueur de fin de classe
+end
